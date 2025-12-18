f27b418cd67d9079b08630acf45199c5af18bd1d
Ivania Zambrano <e1316421690@live.uleam.edu.ec>
2025-12-18 10:24:28 -0500

    final

--- script.js
+++ script.js
@@ -73,9 +73,8 @@ document.getElementById("payButton").addEventListener("click", () => {
         return;
     }
 
-    const storeId = "Adg2MycFGkSfhhyn0pBQ6A";
-    const token = "iGeBjqW1-YGkXcdSZnXer7oOcCt5zgdWtvbd4WsZL1GNAoqui814eJnMq-tc4Z-vzCArdEtAXA6g0eA4gmeYT-YyIPJAm4X7ImBMOVHdrYeHQPLqxSGItG5r6lGi0zLRpZMhNloDysu07ZFLgZObTIMP9ECwVYvkbY6eO1i82PH9vxjs_3-ln8z-OkQLHiY40bqbAIEIbHUmXF_oKX1p5Zvpg1QGWaJUxNtd0_navM6fMj9pueaL-qAo_lc3oep0XmTxiN1KYBisxk5VTbydBBG4G_a4vP_AVpz0BBM4A3-sSCKaVG1akC7cLrIfrvkByCPEcg";
-
+const storeId = "2de59cc3-93f8-49d6-a3fd-d467ced314e0";
+const token = "9Qqj6as__GgqfO9P0_w3GYjDFM8OivtjbxBVLqjxMAEVvDT29E48n2Y14_KcuXUEJXGaTFtqnOJXLlwAIOsYfcQs_LkzyBHiumIt052_oac2mi66Dsn6xAvT_17OgeS1_oUzj-piJrVMbBMJkrHx85E_2Uo5KTNsPT47qVv6kiP18DJBYmOqTp-QDdMB1OmSPIR0uqQtFHtCzUkwXCCYbFyCENQ1lybOzptc7ZBgImWXgFPvIofdeFVjJ2jjQzb-TpsKZpRaA1ubiSL-RspC9m03fYvpztqd-_kVkTWl7GKhRwGnn-V7zhwUJJjm8yRBIbMLnw";
     // Conversión a centavos
     const amountWithTax = Math.round(subtotalVal * 100);   // base gravada
     const tax = Math.round(ivaVal * 100);                  // IVA
